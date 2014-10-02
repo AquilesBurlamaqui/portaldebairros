0f843742c65f84acba22a1281257dd3c6ec0fa21
aquilesair <aquilesair@10.15.15.1>
2014-10-02 14:38:41 -0300

    Primeiro controller teste

---
+++ PortalDeBairros/test/unit/portaldebairros/MeuControllerSpec.groovy
@@ -0,0 +1,20 @@
+package portaldebairros
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(MeuController)
+class MeuControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
