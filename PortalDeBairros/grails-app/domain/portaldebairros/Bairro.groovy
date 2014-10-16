c8febbd37cda65a3c308f3a434824e3215cd9ba7
Tonho <Tonho@Antonio>
2014-10-16 01:51:01 -0300

    add campos ao bairro, falta arrumar a imagem x3

--- PortalDeBairros/grails-app/domain/portaldebairros/Bairro.groovy
+++ PortalDeBairros/grails-app/domain/portaldebairros/Bairro.groovy
@@ -6,5 +6,12 @@ class Bairro {
     byte[] imagem
     
     static constraints = {
+        nome min: 2
+        descricao widget:'textarea'
+        imagem 2048*2048*2
+    }
+    
+    String toString(){
+        return nome
     }
 }
