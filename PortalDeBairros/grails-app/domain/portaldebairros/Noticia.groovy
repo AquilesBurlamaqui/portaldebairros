6258472ebf3b939873dfbb0aa38519b20d1410fa
labinfo <labinfo@LABINFO306.labinfo.ect.ufrn.br>
2014-10-16 18:02:53 -0300

    Criando atributos

--- PortalDeBairros/grails-app/domain/portaldebairros/Noticia.groovy
+++ PortalDeBairros/grails-app/domain/portaldebairros/Noticia.groovy
@@ -1,6 +1,12 @@
 package portaldebairros
 
+import portaldebairros.util.Media
+
 class Noticia {
+    String titulo
+    String descricao
+    Media imagem
+    
 
     static constraints = {
     }
