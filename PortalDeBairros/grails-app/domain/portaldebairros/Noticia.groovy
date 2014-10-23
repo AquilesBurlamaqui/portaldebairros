c2ca60bdaf045a79d1ad668f97deb7259f62a556
labinfo <labinfo@LABINFO306.labinfo.ect.ufrn.br>
2014-10-23 18:01:01 -0300

    Criando formulários

--- PortalDeBairros/grails-app/domain/portaldebairros/Noticia.groovy
+++ PortalDeBairros/grails-app/domain/portaldebairros/Noticia.groovy
@@ -5,9 +5,18 @@ import portaldebairros.util.Media
 class Noticia {
     String titulo
     String descricao
-    Media imagem
+    Usuario usuario
     
+    static hasmany = [imagem: Media]
+      
 
     static constraints = {
+        titulo()
+        descricao widget:"textarea" 
+        
+    }
+    
+    String toString(){
+        return titulo
     }
 }
