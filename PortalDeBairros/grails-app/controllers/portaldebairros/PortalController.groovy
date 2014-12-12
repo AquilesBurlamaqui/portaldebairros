b5bc3f3f638decf12f3a160a1deae608f1ec0e18
Adriana <Adriana@10.15.13.230>
2014-12-12 11:51:49 -0300

    PortalController Aquiles.

--- PortalDeBairros/grails-app/controllers/portaldebairros/PortalController.groovy
+++ PortalDeBairros/grails-app/controllers/portaldebairros/PortalController.groovy
@@ -3,10 +3,11 @@ package portaldebairros
 class PortalController {
 
     def index() {
-    
+        
+        [listaNews: Noticia.list()[0]]
     }
     
     def section() {
         
     }
-}
+} 
